91f75fb489d02574a8d7ccdd372db3b0a482c81f
SarahOorts <r0657122@student.thomasmore.be>
2021-03-26 17:54:33 +0100

    (fix) - change path url from live to dashboard in routes & controllers, update content dashboard & update pug (less li items), update css style.css

--- controllers/api/v1/index.js
+++ controllers/api/v1/index.js
@@ -1,15 +1,17 @@
 function updateStats(req, res) {
-    res.json({
-        status: "succes",
-        message:"hi",
-    });
+    // res.json({
+    //     status: "succes",
+    //     message:"hi",
+    // });
+    res.render("football/update");
 }
 
 function statistics(req,res) {
-    res.json({
-        status: "succes",
-        message:"hiya",
-    });
+    // res.json({
+    //     status: "succes",
+    //     message:"hiya",
+    // });
+    res.render("football/dashboard");
 }
 
 module.exports.updateStats = updateStats;
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('../../../controllers/api/v1/index');
 
 router.get("/update", controller.updateStats);
-router.get("/live", controller.statistics);
+router.get("/dashboard", controller.statistics);
 
 module.exports = router;
