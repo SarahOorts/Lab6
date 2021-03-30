d43d2ab5e0a91d64df8d17e564ccb2c578cb0d0f
SarahOorts <r0657122@student.thomasmore.be>
2021-03-30 17:09:17 +0200

    (fix) - changed location homepage localhost to routes/api/v1/index.js

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -5,4 +5,10 @@ const controller = require('../../../controllers/api/v1/index');
 router.get("/update", controller.updateStats);
 router.get("/dashboard", controller.statistics);
 
+/* GET home page. */
+router.get('/', function (req, res, next){
+    //   router.set("view engine", "pug");
+      res.render('index', { title: 'Lab 6' });
+    });
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const router = express.Router();
 // const routerstats = require("./routes/api/v1/index");
 // const port = 3000;
 
-/* GET home page. */
-router.get('/', function (req, res, next){
-//   router.set("view engine", "pug");
-  res.render('index', { title: 'Lab 6' });
-});
+// /* GET home page. */
+// router.get('/', function (req, res, next){
+// //   router.set("view engine", "pug");
+//   res.render('index', { title: 'Lab 6' });
+// });
 
 // router.use("/api/v1", routerstats);
 
