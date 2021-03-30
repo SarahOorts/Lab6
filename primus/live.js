b95259b40795774f883e65c71fa918b6e917d590
SarahOorts <r0657122@student.thomasmore.be>
2021-03-30 13:15:01 +0200

    (fix) - add spark action content to live.js line 7 - 12

--- primus/live.js
+++ primus/live.js
@@ -3,7 +3,14 @@ const Primus = require("primus");
 function go(server){
     let primus = new Primus(server, {/* options */});
     //primus.save(__dirname +'/primus.js');
-    
+
+    primus.on("action", function (spark){
+        console.log("we have an action");
+        spark.on("data", (data) => {
+            console.log(data);
+            primus.write(data);
+        });
+    });
 }
 
 module.exports.go = go;
